505f54bd1322c3f6218bf6e8fa36149144beea28
32338 <142840950+32338@users.noreply.github.com>
2025-04-09 22:42:04 +0000

    committt

---
+++ crudmusica/src/test/java/ps2/crudmusica/CrudmusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ps2.crudmusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudmusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
